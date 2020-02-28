f5579651d947382178c347cb260b9e00d61ce270
excellis <hho@excelliscorp.com>
2020-02-28 22:49:54 +0800

    Imported SearchResultsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchResultsModule } from 'hani-npm-try';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SearchResultsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
